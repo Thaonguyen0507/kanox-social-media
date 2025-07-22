f3b4d8abe9bc8f679ee195b472dfc812a4300c2a
Nguyen Thao <thtn.0507@gmail.com>
2025-07-23 01:11:21 +0700

    fix import

--- frontend/src/components/posts/TweetInput/TweetInput.jsx
+++ frontend/src/components/posts/TweetInput/TweetInput.jsx
@@ -32,7 +32,7 @@ import { AuthContext } from "../../../context/AuthContext";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import useEmojiList from "../../../hooks/useEmojiList";
-import PlaceAutocomplete from "../location/PlaceAutocomplete";
+import PlaceAutocomplete from "../../location/PlaceAutocomplete";
 
 
 
