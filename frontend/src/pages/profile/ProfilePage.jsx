56ec5c1bc70484626931de169658103f89e0e7f2
Vu Nguyen Khoi <vunguyenkhoi47@gmail.com>
2025-07-28 23:43:29 +0700

    fix

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -472,7 +472,7 @@ function ProfilePage() {
                         </div>
                     </div>
                 </div>
-                header>
+            </header>
 
                 {/* Main Content */}
                 <main className="flex flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-6">
