7e0f360b077220ba58631d17af2b5fcb1057dc9a
Nguyen Thao <thtn.0507@gmail.com>
2025-07-31 14:55:08 +0700

    fix

--- frontend/src/components/utils/FollowActionButton.js
+++ frontend/src/components/utils/FollowActionButton.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import { AuthContext } from "../../context/AuthContext";
 
 function FollowActionButton({ targetId, disabled, onFollowChange, isFollowing, setIsFollowing }) {
